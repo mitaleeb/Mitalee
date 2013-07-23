5b8e167c7cc62df0c1b49634fca5975124fb4e03
Andrew-Burkus <andrewburkus@mccallie.org>
2013-07-23 09:51:47 -0500

    hi

---
+++ C++/Input Test/InputTest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void main () 
+{
+	int a;
+	int b;
+	
+	cout<< "Please Enter an integer \n";
+	cin>> a;
+		cout<< "Please Enter another integer \n";
+	cin>>b;
+	int c = a+b;
+	cout << "The sum of those two integers are: ";
+	cout<<c << "\n";
+
+	system("PAUSE");
+}
